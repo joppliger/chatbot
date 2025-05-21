512dd96509d88c0a917f96e01b7428664f58c38f
ThibaultYVD <thibaultyvard01@gmail.com>
2025-05-21 11:43:05 +0200

    doc_mode

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from app import App
 from modes.load_book_mode import LoadBookMode
 from modes.load_haiku_mode import LoadHaikuMode
 from modes.ask_mode import AskMode
+from modes.doc_mode import DocMode
 
 load_dotenv()
 
@@ -34,5 +35,6 @@ if __name__ == "__main__":
     app.use("load-haiku", LoadHaikuMode)
     app.use("load-book", LoadBookMode)
     app.use("book", BookMode)
+    app.use("doc", DocMode)
 
     app.run()
