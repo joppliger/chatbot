633cfd5c2da715b88c19ae225f01155f55b063ac
joppliger <julien@oppliger.pro>
2025-05-23 10:10:14 +0200

    ok

--- modes/graph_mode.py
+++ modes/graph_mode.py
@@ -5,7 +5,7 @@ from mode import Mode
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.prompts import MessagesPlaceholder
-from langchain_core.messages import SystemMessage
+from langchain_core.messages import SystemMessage, BaseMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import HumanMessagePromptTemplate
 from langchain_core.messages import HumanMessage
@@ -13,6 +13,8 @@ from langchain.agents import tool
 from langchain.agents import AgentExecutor
 from typing import Annotated
 from langchain.agents import create_tool_calling_agent
+from langgraph.graph import MessagesGraph, START, END
+
 
 class GraphMode(Mode):
     def __init__(
@@ -32,4 +34,29 @@ class GraphMode(Mode):
         agent_subparser.add_argument("--model", type=str, default=os.getenv("DEFAULT_MODEL"))
 
     def run(self):
-        print("hello world")
+
+        def chatbot_node(state: list[BaseMessage]) -> BaseMessage:
+            llm = init_chat_model(
+                self.model,
+                model_provider="openai")
+            
+            prompt = ChatPromptTemplate.from_messages([
+                SystemMessage(content="You are a helpful assistant."),
+                HumanMessagePromptTemplate.from_template("{input}"),
+            ])
+            
+            chain = prompt | llm
+            return chain.invoke({ "input": "Quelle est la capitale de la france" })
+
+        graph = MessagesGraph()
+
+        graph.add_node("chatbot", chatbot_node)
+
+        graph.set_entry_point("chatbot")
+        graph.set_finish_point("chatbot")
+
+        app = graph.compile()
+
+        response = app.invoke()
+
+        print(response)
